875bf33297d5d88e01cb44596ab218eec7c7d7d1
Nicholas Nguyen <nicholasnguyen@Nicholass-MacBook-Pro.local>
2020-12-01 00:11:42 -0800

    expresslab2

---
+++ models/skill.js
@@ -0,0 +1,41 @@
+const skills = [
+    {skill: "HTML", level: "7.5/10", experience: "5 weeks"},
+    {skill: "CSS", level: "4.5/10", experience: "5 weeks"},
+    {idx: 2, skill: "JavaScript", level: "9.5/10", experience: "5 weeks"},
+    {skill: "jQuery", level: "0.5/10", experience: "1 week"},
+    {skill: "Express", level: "0.5/10", experience: "3 days"},
+  ];
+  
+  module.exports = {
+    getAll,
+    getOne,
+    create,
+    deleteOne,
+    update
+  }
+  
+  function getAll() {
+    return skills;
+  }
+  
+  function getOne(idx) {
+    return skills[idx];
+  }
+  
+  function create(skill) {
+    if (!skill.skill) skill.skill = "n/a";
+    if (!skill.level) skill.level = "n/a";
+    if (!skill.experience) skill.experience = "n/a";
+    skills.push(skill);
+  }
+  
+  function deleteOne(idx) {
+    skills.splice(idx, 1);
+  }
+  
+  function update(idx, updatedSkill) {
+    if (!updatedSkill.skill) updatedSkill.skill = "n/a";
+    if (!updatedSkill.level) updatedSkill.level = "n/a";
+    if (!updatedSkill.experience) updatedSkill.experience = "n/a";
+    Object.assign(skills[idx], updatedSkill);
+  }
---
+++ routes/skills.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var skillsCtrl = require('../controllers/skills');
+
+// get users listing
+router.get('/', skillsCtrl.index);
+router.get('/new', skillsCtrl.new);
+router.get('/:idx', skillsCtrl.show);
+router.post('/', skillsCtrl.create);
+router.delete('/:idx', skillsCtrl.delete);
+router.get('/:idx/edit', skillsCtrl.edit);
+router.put('/:idx', skillsCtrl.update);
+
+module.exports = router;
